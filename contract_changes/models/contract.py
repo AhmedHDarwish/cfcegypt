3dc6e997a0faa41bf1af1533523db793e34db72f
hatem1055 <odoo@eupd3.odoo.com>
2022-04-14 22:05:21 +0000

    contract fields

--- contract_changes/models/contract.py
+++ contract_changes/models/contract.py
@@ -5,6 +5,27 @@ from odoo import models, fields, api
 
 class HrContract(models.Model):
     _inherit = 'hr.contract'
-    transportaion = fields.Monetary()
-    other_allowances = fields.Monetary()
-        
+    transportation_type = fields.Selection(selection = [('cash','Cash'),('company_car','Company Car'),('company_bus','Company Bus')])
+    transportaion_amount = fields.Monetary()
+    housing_type = fields.Selection(selection = [('cash','Cash'),('single_house','Single House'),('shared_house','Shared House')])
+    housing_amount = fields.Monetary()
+    #medical
+    has_medical_insurance = fields.Boolean()
+    medical_class = fields.Selection(selection = [('a','A'),('b','B'),('c','C')])
+    monthly_fees = fields.Monetary()
+    medical_type = fields.Selection(selection = [('employee_pay','Employee Pay'),('company_pay','Company Pay'),('employee_pay_perc','Employee Pay %')])
+    medical_percentage = fields.Float()
+    medical_attachment = fields.Binary()
+    #social
+    has_social_insurance = fields.Boolean()
+    social_isurance_number = fields.Char()
+    social_isurance_date = fields.Date()
+    social_isurance_attachment = fields.Binary()
+    #allowances
+    allowance_1 = fields.Monetary()
+    allowance_2 = fields.Monetary()
+    allowance_3 = fields.Monetary()
+    allowance_4 = fields.Monetary()
+    
+    
+    
